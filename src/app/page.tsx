1e2987a3c3ce094d95dcd325ba45c68dd350f2db
Hans Teh <73771314+HansT01@users.noreply.github.com>
2024-05-10 18:05:56 +1000

    cheese data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import Image from 'next/image'
+import cheeses from '../cheeses.json'
+
 export default function Home() {
   return (
-    <main className='bg-background text-background-fg flex min-h-svh items-center justify-center'>
-      <h1 className='text-4xl'>Hello world!</h1>
+    <main className='bg-background text-background-fg flex min-h-svh flex-col items-center justify-center gap-8'>
+      <h1 className='text-4xl'>PZ Cheeseria</h1>
+      <div className='flex flex-col gap-4'>
+        {cheeses.map((cheese) => {
+          return (
+            <div id={cheese.id} className='flex gap-4'>
+              <Image src={`/images/${cheese.id}.jpg`} alt={cheese.name} width={200} height={200} />
+              <h2 className='text-2xl capitalize'>{cheese.name}</h2>
+            </div>
+          )
+        })}
+      </div>
     </main>
   )
 }
