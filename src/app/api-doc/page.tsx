025bcb994862a6b88a2f61478e8f4419f66c7c2f
Hans Teh <73771314+HansT01@users.noreply.github.com>
2024-05-10 22:43:46 +1000

    moved swagger spec

--- src/app/api-doc/page.tsx
+++ src/app/api-doc/page.tsx
@@ -1,5 +1,20 @@
-import { getApiDocs } from '@/utils/swagger'
+import { createSwaggerSpec } from 'next-swagger-doc'
 import ReactSwagger from './react-swagger'
+
+const getApiDocs = async () => {
+  const spec = createSwaggerSpec({
+    apiFolder: 'src/app/api',
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Next Swagger API Example',
+        version: '1.0',
+      },
+      security: [],
+    },
+  })
+  return spec
+}
 
 const APIDoc = async () => {
   const spec = await getApiDocs()
